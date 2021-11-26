54d193dd18368e1c817d97260c79fee14495c7a4
Rusty Green <rustymgreen@gmail.com>
2021-11-26 15:29:59 -0500

    chore: updates script to include cloud-init reset

--- setup.sh
+++ setup.sh
@@ -8,9 +8,14 @@ sudo truncate -s 0 /etc/machine-id
 # Install cloud init packages.
 sudo apt install cloud-init cloud-initramfs-copymods cloud-initramfs-dyn-netconf
 
-# Update existing packages
+# Update existing packages.
 sudo apt upgrade
+
+# Reset cloud init just in case this VM was generated from a previously ran cloud-init image.
+cloud-init clean -l
 
 # Potentially clear/cleanup packages.
 sudo apt clean
 sudo apt autoremove
+
+echo "VM prepare script completed succesfullly. This VM can now be converted to a template"
